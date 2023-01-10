890736faa76a04f6b7c5b2495debbbfce7cb501a
lemonci <hongyanchangfei@gmail.com>
2023-01-10 15:41:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('My Parents New Healthy Diner')
+st.title('My Mom\'s New Healthy Diner')
 
 st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,4 +12,9 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 st.dataframe(my_fruit_list)
